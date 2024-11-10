7d4eef812c2c5acd217dd8438fe8de2fefaeb696
Syed Muhammad Ali Gardezi <smagardezii@gmail.com>
2024-11-10 17:17:24 -0500

    initial store structure

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,77 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Product } from 'src/app/models/product.model';
+import { CartService } from 'src/app/services/cart.service';
+import { StoreService } from 'src/app/services/store.service';
+
+const ROWS_HEIGHT: { [id: number]: number } = {
+  1: 236,
+  3: 335,
+  4: 350
+}
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html' ,
+  styles: [
+  ]
+})
+export class HomeComponent implements OnInit, OnDestroy {
+
+  cols = 3;
+  rowHeight = ROWS_HEIGHT[this.cols];
+  category: string | undefined;
+  products: Array<Product> | undefined;
+  sort = 'desc';
+  count="12";
+  productsSubscription: Subscription | undefined;
+
+  constructor(private cartService: CartService, private storeService: StoreService) { }
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts(): void {
+    this.storeService.getAllProducts(this.count, this.sort)
+      .subscribe((_products) => {
+        this.products = _products
+      })
+  }
+
+  oncolumnsCountChange(colsNum: number): void {
+    this.cols = colsNum;
+    this.rowHeight = ROWS_HEIGHT[this.cols];
+  }
+  
+
+  onShowCategory(newCategory: string): void {
+    this.category = newCategory;
+  }
+
+  onAddToCart(product: Product): void {
+    this.cartService.addToCart({
+      product: product.image,
+      name: product.title,
+      price: product.price,
+      quantity: 1,
+      id: product.id
+    })
+  }
+
+  onItemsCountChange(newCount: number): void {
+    this.count = newCount.toString();
+    this.getProducts()
+  }
+
+  onSortChange(sort: string): void {
+    this.sort = sort;
+    this.getProducts()
+  }
+
+  ngOnDestroy(): void {
+    if (this.productsSubscription)
+      this.productsSubscription.unsubscribe();
+  }
+
+}
